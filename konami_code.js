84fc8d5512f2cdaa7a94397b79a7b16a09116fad
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-17 14:27:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,26 @@ const codes = [
 function init() {
   // your code here
 }
+
+
+
+let index = 0;
+
+function init() {
+	document.body.addEventListener('keydown', function event(e)
+	{
+	  let key = e.key;
+	  
+	  if (key === codes[index]) {
+	    index++;
+	  }
+	  
+	  if (index === codes.length) {
+	    alert("Congratulations, you entered the Konami code!");
+	    
+	    index = 0;
+	  } else {
+	    index = 0;
+	  }
+	  
+	  })}
