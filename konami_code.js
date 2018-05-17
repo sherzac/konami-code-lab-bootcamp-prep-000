2194fa63823b41b1e245fd75a61d7276f8a380b8
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-17 14:38:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -18,19 +18,21 @@ const codes = [
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  var index = 0
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which)
-
+  var index = 0;
+  document.addEventListener("keydown", konamiCode)
+  function konamiCode(e) {
+    const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
-      index++;
-
-      if (index === code.length - 1) {
-        alert("YOU DID IT!");
+      index ++;
+      if (index === code.length) {
+        alert("Wow. Such Secret. So Konami. Wow.");
         index = 0;
       }
     } else {
       index = 0;
+
     }
-  });
+  }
+  // Write your JavaScript code inside the init() function
+
 }
